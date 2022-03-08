d8cd8e18dd2647944c183d6d61b7c463a6a23b04
Bruke Wondessen <bwondessen23@gmail.com>
2022-03-08 16:42:09 -0600

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Bruke on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
